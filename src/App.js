a5f7bf4652dda76b3a1c565ec240caa107117af4
Jeremia Pane <jeripane@gmail.com>
2022-06-20 16:31:22 +0700

    refactoring folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import TodoForm from './components/TodoForm';
 
 function App() {
     return ( 
-      <div className = "App" >
-        <h1> To Do App </h1>  
-        </div>
+
+      <div className = "todo-App" >
+      <TodoForm/>
+      </div>
     );
 }
 

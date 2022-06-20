e29430e8abcb7229d658b1332d43a94cb8e146f9
Jeremia Pane <jeripane@gmail.com>
2022-06-20 16:48:39 +0700

    Add handle

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,10 +1,25 @@
+// ambil data react dari library
 import React, { useState } from 'react'
 
-function TodoForm() {
+// bikin component form
+function TodoForm(props) {
     const [input,setInput] = useState('');
+    const handleChange = e =>{
+        setInput(e.target.value);
+    }
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+
+        // this.props.onSubmit({
+        //     id: Math.floor(Math.random()*10000),
+        //     text: input
+        // }); 
+        setInput('');
+    };
   return (
-    <form>
-        <input type='text' placeholder='Add to do' value={input} name='text' className='todo-input'/>
+    <form className='todo-form' onSubmit={handleSubmit}>
+        <input type='text' placeholder='Add to do' value={input} name='text' className='todo-input' onChange={handleChange}/>
         <button className='todo-button'>Add To do</button>
     </form>
   )
